d89895e394ecf13d13b5a7476ced490f82b49e08
SnowLegend-star <1716438226@qq.com>
2024-04-26 15:43:36 +0800

    完成part01: Memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -8,6 +8,7 @@
 #include "spinlock.h"
 #include "riscv.h"
 #include "defs.h"
+// #include<stddef.h>
 
 void freerange(void *pa_start, void *pa_end);
 
@@ -21,12 +22,26 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  int sz; //记录当前CPU的freelist有多少个PAGE
+} kmem[NCPU];
+
+int
+cpuid_Modify(){
+  //用原子操作来获取当前CPU
+  push_off();
+  int cpuid_Cur=cpuid();
+  pop_off();
+  return cpuid_Cur;
+}
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  int i;
+  for(i=0; i<NCPU; i++){
+    initlock(&kmem[i].lock, "kmem");
+    kmem[i].sz=0;
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +71,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  //初始状态下，当前运行的cpuid一下子获得所有的空闲空间
+  // int cpuid=cpuid_Modify();
+  int cpuid_Cur=cpuid();
+  acquire(&kmem[cpuid_Cur].lock);
+  r->next = kmem[cpuid_Cur].freelist;
+  kmem[cpuid_Cur].freelist = r;
+  kmem[cpuid_Cur].sz++;
+  release(&kmem[cpuid_Cur].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,14 +90,58 @@ void *
 kalloc(void)
 {
   struct run *r;
+  struct run *node_steal;
+  int i;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  // int cpuid=cpuid_Modify();
+  int cpuid_Cur=cpuid();
+
+  acquire(&kmem[cpuid_Cur].lock);
+  r = kmem[cpuid_Cur].freelist;
+
+  //如果当前CPU的freelist为空
+  if(!r)
+  {
+    for (i = 0; i < NCPU; i++)
+    {
+      if (i == cpuid_Cur)
+        continue;
+      acquire(&kmem[i].lock);
+      // if (kmem[i].freelist && kmem[i].freelist->next)
+      if (kmem[i].freelist)
+      {
+        // 从第一个不空的CPU的freelist截取后半段，同时这个freelist得大于等于两个PAGE
+        struct run *tmp = kmem[i].freelist;
+        node_steal = kmem[i].freelist;
+        for (int j = 1; j < kmem[i].sz / 2; j++)   //西巴，千算万算没看懂这里混进去了i
+        {
+          // tmp=tmp->next;
+        }
+        // 已经遍历到了当前freelist的中间部分
+        kmem[i].freelist=tmp->next;
+        tmp->next = (void *)0;        // 截断当前的freelist
+        r = node_steal;
+        kmem[i].sz-=kmem[i].sz/2;
+        kmem[cpuid_Cur].sz=kmem[i].sz/2;
+        release(&kmem[i].lock);
+        break;                        //找到合适的freelist就直接退出
+      }
+      release(&kmem[i].lock);
+    }
+  }
+  // r=node_steal; //可恶，这句话怎么写在if(!r)外面了
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpuid_Cur].freelist = r->next;
+  kmem[cpuid_Cur].sz--;
+  release(&kmem[cpuid_Cur].lock);
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+
+  // struct run* xx=r;
+  // if(xx)
+  //   printf("xx: %p", xx);
   return (void*)r;
 }
